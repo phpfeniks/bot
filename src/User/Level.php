ae61d615df5a250100850c21ef1d1cfb69ec7cbb
Audun Larsen <larsen@xqus.com>
2022-11-04 20:53:01 +0100

    Improve /level command

--- src/User/Level.php
+++ src/User/Level.php
@@ -9,6 +9,7 @@ use Discord\Parts\Embed\Embed;
 use Discord\Parts\Interactions\Interaction;
 use Feniks\Bot\Models\Guild as GuildModel;
 use Feniks\Bot\Models\User;
+use Illuminate\Support\Carbon;
 
 class Level
 {
@@ -36,6 +37,15 @@ class Level
         return $currentRank;
     }
 
+    public function levelRequirement($level)
+    {
+        if(isset($this->ranks()[$level]['requirement'])) {
+            return $this->ranks()[$level]['requirement'];
+        }
+
+        return false;
+    }
+
     public function ranks()
     {
         return $this->guild->settings()->get('points.ranks', []);
@@ -46,13 +56,46 @@ class Level
         $guild = GuildModel::where('discord_id', $this->guild->id)->first();
         $userGuild = $this->user->guilds()->where('guilds.id', $this->guild->id)->first();
 
+        $requirment['current'] = $this->levelRequirement($this->level($userGuild->pivot->points));
+        $requirment['next'] = $this->levelRequirement($this->level($userGuild->pivot->points)+1);
+        $requirment['progress'] = false;
+
+
         $embed = new \Feniks\Bot\Embed($this->guild);
         $embed
-            ->title(":clipboard: Your level")
+            ->title(":sparkles: Your level")
             ->description("Below is a summary of your level and points")
-            ->field('Total XP', $userGuild->pivot->points, true)
-            ->field('Current level', $this->level($userGuild->pivot->points), true)
+            ->field(':chart_with_upwards_trend: Total XP', $userGuild->pivot->points, true)
+            ->field(':trophy: Current level', $this->level($userGuild->pivot->points), true)
         ;
+
+        if($requirment['next'] !== false) {
+            $xpForLevel = $requirment['next'] - $requirment['current'];
+            $xpAfterLevelUp = $userGuild->pivot->points - $requirment['current'];
+            $precent = round(($xpAfterLevelUp / $xpForLevel) * 100);
+            $progress = "`{$xpAfterLevelUp}/{$xpForLevel} - {$precent}%`";
+            $embed->field(
+                ':shield: Progress to next level',
+                $progress
+            );
+        }
+
+        $seasons = $this->guild->seasons()
+            ->orderBy('end_date', 'desc')
+            ->whereDate('start_date', '<=', Carbon::now('UTC'))
+            ->whereDate('end_date', '>=', Carbon::now('UTC')->subWeek())
+            ->get();
+
+        foreach($seasons as $season) {
+            $userseason = $this->user->seasons()->where('seasons.id', $season->id)->first();
+
+            $embed->field(
+                ":ticket: Season: *{$season->name}*",
+                "Total `{$userseason->pivot->points} XP`",
+                true
+            );
+
+        }
 
 
         return new Embed($this->discord, $embed->toArray());
