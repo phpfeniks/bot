aab2fa9a754dac1ccbc20a9decccaba52f642e4e
Audun Larsen <larsen@xqus.com>
2022-11-16 09:37:58 +0100

    Add level command

---
+++ src/Commands/Level.php
@@ -0,0 +1,33 @@
+<?php
+
+
+namespace Feniks\Bot\Commands;
+
+
+use Discord\Builders\MessageBuilder;
+use Discord\Parts\Interactions\Command\Option;
+use Discord\Parts\Interactions\Interaction;
+
+class Level extends Command
+{
+    protected $name = 'level';
+    protected $description = 'Show the user information card for you or any other user';
+    protected $options = [
+        [
+            'name' => 'user',
+            'description' => 'User to display',
+            'required' => false,
+            'type' => Option::USER,
+            'autocomplete' => false
+        ]
+    ];
+    public function handle(Interaction $interaction)
+    {
+        $level = new \Feniks\Bot\User\Level($interaction, $this->discord);
+        $showLevel = $level->showLevel();
+        if(! $showLevel) {
+            dump('nooo');
+            $interaction->respondWithMessage(MessageBuilder::new()->setContent(':information_source:  I have no information about this user.'));
+        }
+    }
+}
