e62826a90d9f54acfd94357ee936f354fe3e5a82
Audun Larsen <larsen@xqus.com>
2022-11-20 20:29:16 +0100

    Use hashids for season ids in commands. Fixes #11

--- src/Guild/Scoreboard.php
+++ src/Guild/Scoreboard.php
@@ -9,105 +9,108 @@ use Discord\Parts\Embed\Embed;
 use Feniks\Bot\Models\Guild as GuildModel;
 use Discord\Parts\Interactions\Interaction;
 use Feniks\Bot\Models\Season;
+use Hashids\Hashids;
 
 class Scoreboard
 {
-  private $guild;
-  private $discord;
-  private $season;
-  private $scoreboard = [];
+    private $guild;
+    private $discord;
+    private $season;
+    private $scoreboard = [];
 
-  public function __construct(Interaction $interaction, Discord $discord, $season = null)
-  {
-    $this->guild = GuildModel::where('discord_id', $interaction->guild_id)->first();
-    $this->discord = $discord;
-    $this->season = $season;
+    public function __construct(Interaction $interaction, Discord $discord, $season = null)
+    {
+        $this->guild = GuildModel::where('discord_id', $interaction->guild_id)->first();
+        $this->discord = $discord;
+        $this->season = $season;
 
-    if($season !== null ) {
-      $this->season = $this->guild->seasons()->where('id', $season)->first();
-      if(! $this->season) {
-        $interaction->respondWithMessage(MessageBuilder::new()->setContent(':red_square: **No such season**!* Check `/seasons` for available seasons on this server.'));
-      }
-    }
-  }
-
-  private function getUsers()
-  {
-
-    if($this->season !== null) {
-      return $this->season->users()
-        ->orderByPivot('points', 'desc')->limit(13)->get();
-    }
-    return $this->guild->users()
-      ->orderByPivot('points', 'desc')->limit(13)->get();
-  }
-
-  private function create()
-  {
-    $position = 1;
-    $positionIcons = [
-      1 => ' :trophy: ',
-      2 => ' :second_place: ',
-      3 => ' :third_place: ',
-    ];
-    foreach($this->getUsers() as $user) {
-      if(isset($positionIcons[$position])) {
-        $this->scoreboard[] .= "{$positionIcons[$position]} <@{$user->discord_id}> `{$user->pivot->points} XP`";
-      } else {
-        $this->scoreboard[] .= "**{$position}.** <@{$user->discord_id}> `{$user->pivot->points} XP`";
-      }
-
-      $position++;
-    }
-  }
-
-  private function top3()
-  {
-    return implode(PHP_EOL, array_slice($this->scoreboard, 0, 3));
-
-  }
-
-  private function honerable()
-  {
-    return implode(PHP_EOL, array_slice($this->scoreboard, 3));
-  }
-
-  public function getEmbed()
-  {
-    $this->create();
-
-    $embed = new \Feniks\Bot\Embed($this->guild);
-
-    if($this->season === null) {
-      $embed
-        ->title(':clipboard: Scoreboard')
-        ->description("Below is the total scores for *{$this->guild->name}*");
-    } else {
-      $embed
-        ->title(':clipboard: Season scoreboard')
-        ->description("Below is the scores for *{$this->season->name}*");
+        if($season !== null ) {
+            $hashids = new Hashids();
+            $season = $hashids->decode($season);
+            $this->season = $this->guild->seasons()->where('id', $season)->first();
+            if(! $this->season) {
+                $interaction->respondWithMessage(MessageBuilder::new()->setContent(':red_square: **No such season**!* Check `/seasons` for available seasons on this server.'));
+            }
+        }
     }
 
+    private function getUsers()
+    {
 
-    $embed
-      ->field(
-        'Top 3 :speech_balloon:',
-        $this->top3(),
-        true
-      )
-      ->field(
-        'Honorable mentions :speech_balloon:',
-        $this->honerable() ? $this->honerable() : 'None',
-        true
-      );
+        if($this->season !== null) {
+            return $this->season->users()
+                ->orderByPivot('points', 'desc')->limit(13)->get();
+        }
+        return $this->guild->users()
+            ->orderByPivot('points', 'desc')->limit(13)->get();
+    }
 
-      if(! $this->guild->isConfigured()) {
-          $embed->field(
-              ":robot: Missing configuration",
-              ":x: **Basic configuration for Feniks is missing.** You should tell your server administrator to [configure Feniks](https://docs.feniksbot.com/#/admin/quickstart)."
-          );
-      }
+    private function create()
+    {
+        $position = 1;
+        $positionIcons = [
+            1 => ' :trophy: ',
+            2 => ' :second_place: ',
+            3 => ' :third_place: ',
+        ];
+        foreach($this->getUsers() as $user) {
+            if(isset($positionIcons[$position])) {
+                $this->scoreboard[] .= "{$positionIcons[$position]} <@{$user->discord_id}> `{$user->pivot->points} XP`";
+            } else {
+                $this->scoreboard[] .= "**{$position}.** <@{$user->discord_id}> `{$user->pivot->points} XP`";
+            }
 
-    return new Embed($this->discord, $embed->toArray());
-  }
+            $position++;
+        }
+    }
+
+    private function top3()
+    {
+        return implode(PHP_EOL, array_slice($this->scoreboard, 0, 3));
+
+    }
+
+    private function honerable()
+    {
+        return implode(PHP_EOL, array_slice($this->scoreboard, 3));
+    }
+
+    public function getEmbed()
+    {
+        $this->create();
+
+        $embed = new \Feniks\Bot\Embed($this->guild);
+
+        if($this->season === null) {
+            $embed
+                ->title(':clipboard: Scoreboard')
+                ->description("Below is the total scores for *{$this->guild->name}*");
+        } else {
+            $embed
+                ->title(':clipboard: Season scoreboard')
+                ->description("Below is the scores for *{$this->season->name}*");
+        }
+
+
+        $embed
+            ->field(
+                'Top 3 :speech_balloon:',
+                $this->top3(),
+                true
+            )
+            ->field(
+                'Honorable mentions :speech_balloon:',
+                $this->honerable() ? $this->honerable() : 'None',
+                true
+            );
+
+        if(! $this->guild->isConfigured()) {
+            $embed->field(
+                ":robot: Missing configuration",
+                ":x: **Basic configuration for Feniks is missing.** You should tell your server administrator to [configure Feniks](https://docs.feniksbot.com/#/admin/quickstart)."
+            );
+        }
+
+        return new Embed($this->discord, $embed->toArray());
+    }
 }
--- src/Season/Overview.php
+++ src/Season/Overview.php
@@ -8,36 +8,39 @@ use Discord\Builders\MessageBuilder;
 use Discord\Parts\Interactions\Interaction;
 use Discord\Parts\Embed\Embed;
 use Feniks\Bot\Models\Guild as GuildModel;
+use Hashids\Hashids;
 
 class Overview
 {
 
-  private $interaction;
-  private $discord;
+    private $interaction;
+    private $discord;
 
-  public function __construct(Interaction $interaction, Discord $discord)
-  {
-    $this->interaction = $interaction;
-    $this->discord = $discord;
-  }
-
-  public function all()
-  {
-    $guild = GuildModel::where('discord_id', $this->interaction->guild_id)->first();
-    $seasons = $guild->seasons()->orderBy('end_date', 'desc')->get();
-
-    $embed = new \Feniks\Bot\Embed($guild);
-    $embed
-      ->title(":clipboard: All seasons")
-      ->description("Below is all past and future seasons for this server");
-
-    foreach($seasons as $season) {
-      $embed->field(
-        "$season->name / {$season->start_date} to {$season->end_date}",
-        "{$season->description}\n `/season {$season->id}` \n-------------------------"
-      );
+    public function __construct(Interaction $interaction, Discord $discord)
+    {
+        $this->interaction = $interaction;
+        $this->discord = $discord;
     }
-    return new Embed($this->discord, $embed->toArray());
-  }
+
+    public function all()
+    {
+        $guild = GuildModel::where('discord_id', $this->interaction->guild_id)->first();
+        $seasons = $guild->seasons()->orderBy('end_date', 'desc')->get();
+
+        $embed = new \Feniks\Bot\Embed($guild);
+        $embed
+            ->title(":clipboard: All seasons")
+            ->description("Below is all past and future seasons for this server");
+
+        $hashids = new Hashids();
+
+        foreach($seasons as $season) {
+            $embed->field(
+                "$season->name / {$season->start_date} to {$season->end_date}",
+                "{$season->description}\n `/season {$hashids->encode($season->id)}` \n-------------------------"
+            );
+        }
+        return new Embed($this->discord, $embed->toArray());
+    }
 
 }
